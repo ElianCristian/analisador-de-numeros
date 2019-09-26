ac7fdef55e384ee8142b4065b3270914e46a9ccd
Elian <cris.ufam2012@gmail.com>
2019-09-26 14:57:40 -0400

    Total de elementos, maior, menor, soma e media

--- scripts.js
+++ scripts.js
@@ -3,12 +3,6 @@ let lista = document.querySelector('select#lista')
 let resultado = document.querySelector('div#res')
 let vetor = []
 
-/* function Adicionar () {
-   let numero = Number(num.value)
-    console.log(numero)  
-}
-*/
-
 function isNumero(n) {
     if (Number(n) >=1 && Number(n) <= 100) {
         return true
@@ -30,12 +24,42 @@ function Adicionar() {
         vetor.push(Number(num.value))
         let pos = vetor.length
         let item = document.createElement('option')
-        item.text = `Inserido o ${vetor[pos -1]}`
+        item.text = `${pos} - Inserido o ${num.value}`
         lista.appendChild(item)
+        resultado.innerHTML = '' // caso eu tenha selecionado "Finalizar" na tela e em seguida adicionar um numero
     }
     else{
         alert(`[${Number(num.value)}] Valor invalido ou já se encontra na lista`)
     }
     num.value = ''
     num.focus()
+}
+
+function Finalizar() {
+    if (vetor.length == 0) {
+        alert('A lista está vazia')
+    } else {
+        let totElementos = vetor.length
+        let maior = vetor[0]
+        let menor = vetor[0]
+        let soma = 0
+        
+        for(let pos in vetor){
+            soma += vetor[pos]
+
+            if (vetor[pos] > maior) {
+                maior = vetor[pos]
+            }
+            if (vetor[pos] < menor) {
+                menor = vetor[pos]
+            }
+        }
+
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p> Total de elementos: ${totElementos} </p>`
+        resultado.innerHTML += `<p> Maior valor encontrado: ${maior}</p>`
+        resultado.innerHTML += `<p>Menor valor encontrado: ${menor}</p>`
+        resultado.innerHTML += `<p>Soma de todos os valores: ${soma}</p>`
+        resultado.innerHTML += `<p>Média dos valores: ${soma/totElementos}</p>`
+    }
 }
