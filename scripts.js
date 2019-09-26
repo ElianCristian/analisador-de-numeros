0965e2e68bea7f3c48b534987447063146f3f661
Elian <cris.ufam2012@gmail.com>
2019-09-26 13:47:08 -0400

    Vericacao dos intervalos permitidos na insercao

--- scripts.js
+++ scripts.js
@@ -1,10 +1,35 @@
-function Analisar() {
-    let num = document.getElementById('num')
-    let numero = Number(num.value)
-    if (num.value == '' || num.value > 100) {
-        alert('Preencha o campo corretamente')
+let num = document.getElementById('num')
+let lista = document.querySelector('select#lista')
+let resultado = document.querySelector('div#res')
+let vetor = []
+
+/* function Adicionar () {
+   let numero = Number(num.value)
+    console.log(numero)  
+}
+*/
+
+function isNumero(n) {
+    if (Number(n) >=1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+function inLista(n, vet) {
+    if (vet.indexOf(Number(n)) != -1){ // verifica se o valor n esta no vetor
+        return true
     }
     else{
-        alert('Tudo certo')
+        return false
+    }
+}
+
+function Adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, vetor)){
+        alert('Tudo Ok')
+    }
+    else{
+        alert('Valor invalido ou já se encontra na lista')
     }
 }
